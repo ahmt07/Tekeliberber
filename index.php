41ce96ed6a04fdb1d70736b824ae0ff7d4c626e6
ahmt07 <59864560+ahmt07@users.noreply.github.com>
2026-03-10 15:16:10 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
-declare(strict_types=1);
-session_start();
-date_default_timezone_set('Europe/Istanbul');
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 
+echo "PHP çalışıyor";
 /* =========================================================
    AYARLAR
    ========================================================= */
